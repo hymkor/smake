503ff2db4b0cdfec2c2c6be96098ed3bc890b446
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-10-22 20:43:33 +0900

    Remove (split-sequence)

--- orig.go
+++ orig.go
@@ -8,36 +8,6 @@ import (
 	"github.com/nyaosorg/go-windows-su"
 )
 
-func newSequenceBuilder(s gm.Node) gm.SeqBuilder {
-	if _, ok := s.(gm.UTF8String); ok {
-		return &gm.UTF8StringBuilder{}
-	}
-	if _, ok := s.(gm.UTF32String); ok {
-		return &gm.UTF32StringBuilder{}
-	}
-	return &gm.ListBuilder{}
-}
-
-func funSplitSequence(ctx context.Context, w *gm.World, list []gm.Node) (gm.Node, error) {
-	sep := list[0]
-	str := list[1]
-
-	var result gm.ListBuilder
-	subSeq := newSequenceBuilder(list[1])
-
-	gm.SeqEach(str, func(value gm.Node) error {
-		if value.Equals(sep, gm.EQUAL) {
-			result.Add(subSeq.Sequence())
-			subSeq = newSequenceBuilder(str)
-		} else {
-			subSeq.Add(value)
-		}
-		return nil
-	})
-	result.Add(subSeq.Sequence())
-	return result.Sequence(), nil
-}
-
 func funShellExecute(ctx context.Context, w *gm.World, list []gm.Node) (gm.Node, error) {
 	var sup su.Param
 	sup.Show = su.SHOWNORMAL
